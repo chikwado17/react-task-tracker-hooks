5870e8df014d18a96a5461eb8de2ac45c2bbc1e8
chikwado17 <chikwadonworie@gmail.com>
2021-06-10 17:02:12 +0100

    header component completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>
-      <h1>React Header</h1>
+    <div className="container">
+      <Header/>
     </div>
   )
 }
